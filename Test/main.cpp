3dac70b679997bea64058d6afe4c17639ddcae2d
Daria Kozlova <daria@MacBook-Pro-Daria.local>
2018-11-07 10:05:13 +0300

    Added Tests for StrandPoint and Strand methods

--- Test/main.cpp
+++ Test/main.cpp
@@ -58,3 +58,63 @@ TEST(StrandPoint, setPosition)
     EXPECT_FLOAT_EQ(pos.y, -0.2f);
     EXPECT_FLOAT_EQ(pos.z, 12.303f);
 }
+
+TEST(StrandPoint, setVelocity)
+{
+    StrandPoint p;
+    p.setVelocity({0.0f, -0.2f, 0.0f});
+    auto vel = p.getVelocity();
+    EXPECT_FLOAT_EQ(vel.x, 0.0f);
+    EXPECT_FLOAT_EQ(vel.y, -0.2f);
+    EXPECT_FLOAT_EQ(vel.z, 0.0f);
+}
+
+TEST(StrandPoint, setMass)
+{
+    StrandPoint p;
+    p.setMass(1.0f);
+    auto mass = p.getMass();
+    EXPECT_FLOAT_EQ(mass, 1.0f);
+}
+
+TEST(StrandPoint, setRadius)
+{
+    StrandPoint p;
+    p.setRadius(1.0f);
+    auto radius = p.getRadius();
+    EXPECT_FLOAT_EQ(radius, 1.0f);
+}
+
+TEST(StrandPoint, setRegionWidth)
+{
+    StrandPoint p;
+    p.setRegionWidth(3);
+    auto region = p.getRegionWidth();
+    EXPECT_EQ(region, 3);
+}
+
+TEST(StrandPoint, setPointId)
+{
+    StrandPoint p;
+    p.setPointId(10);
+    auto id = p.getPointId();
+    EXPECT_EQ(id, 10);
+}
+
+// ----------- STRAND ------------
+
+TEST(Strand, setStrandId)
+{
+    Strand s;
+    s.setStrandId(20);
+    auto s_id = s.getStrandId();
+    EXPECT_EQ(s_id, 20);
+}
+
+TEST(Strand, getNumOfPointsInStrand)
+{
+    Strand s;
+    auto n_p = s.getNumberOfPointInStrand();
+    EXPECT_EQ(n_p, 0);
+}
+
